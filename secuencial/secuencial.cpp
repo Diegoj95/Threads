0de1f5300104a1f272d65e06a1fc710e846efaee
Diegoj95 <diegojimenez377@gmail.com>
2023-11-30 19:33:12 -0300

    parametros de entrada y chrono en secuencial

--- secuencial/secuencial.cpp
+++ secuencial/secuencial.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <chrono>
 
 using namespace cv;
 using namespace std;
+using namespace chrono;
 
-int main() {
-    Mat image = imread("../imagenes/imagen.jpg", IMREAD_COLOR);
-
-    if (image.empty()) {
-        cout << "No se encontró la imagen.\n";
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Uso: " << argv[0] << " <nombre_imagen_color> <nombre_imagen_grises>\n";
         return -1;
     }
 
-    // Nueva matriz
+    // Cargar la imagen
+    Mat image = imread(argv[1], IMREAD_COLOR);
+
+    if (image.empty()) {
+        cout << "No se encontró la imagen a color.\n";
+        return -1;
+    }
+
+    // Inicio
+    auto start = high_resolution_clock::now();
+
+    // Matriz en escala de grises
     Mat grayImage(image.rows, image.cols, CV_8UC1);
 
     for (int r = 0; r < image.rows; r++) {
@@ -22,17 +33,24 @@ int main() {
             // Calcular la luminosidad
             uchar luminosity = static_cast<uchar>(0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0]);
 
-            // Asignar el valor de luminosidad al píxel en la nueva imagen en escala de grises
             grayImage.at<uchar>(r, c) = luminosity;
-
-            //cout << static_cast<int>(luminosity) << " ";
         }
-        cout << endl;
     }
 
-    // Mostrar la imagen en escala de grises
+    // Fin
+    auto stop = high_resolution_clock::now();
+
+    // Calcular el tiempo de ejecución en milisegundos
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    cout << "Tiempo de ejecución: " << duration.count() << " ms\n";
+
+    // Guardar la imagen
+    imwrite(argv[2], grayImage);
+
+    // Mostrar la imagen
     imshow("Imagen en escala de grises", grayImage);
-    waitKey(0);
+    waitKey(2000);
 
     return 0;
 }
