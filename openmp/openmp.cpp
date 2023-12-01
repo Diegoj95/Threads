d5b73a5045ae1489640011e1287f440a1421077a
Tomas <108380952+Totomas1@users.noreply.github.com>
2023-12-01 16:41:53 -0300

    openMP

--- openmp/openmp.cpp
+++ openmp/openmp.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <omp.h>
 
 using namespace cv;
 using namespace std;
 
 int main() {
 
-    Mat image = imread("../imagenes/imagen.jpg", IMREAD_COLOR);
+    Mat image = imread("C:\Users\Tomás\Documents\Code\Threads-master\imagenes", IMREAD_COLOR);
 
     if (image.empty()) {
         cout << "No se encontró imagen.\n";
         return -1;
     }
 
+    int num_hilos;
+    
+    do {
+        cout << "Ingrese la cantidad de hilos (1-16): ";
+        cin >> num_hilos;
+
+        if (num_hilos < 1 || num_hilos > 16) {
+            cout << "Número de hilos no válido. Debe estar entre 1 y 16.\n";
+        }
+    } while (num_hilos < 1 || num_hilos > 16);
+
+    // hilos
+    omp_set_num_threads(num_hilos);
+
+    #pragma omp parallel for
     for (int r = 0; r < image.rows; r++) {
         for (int c = 0; c < image.cols; c++) {
             Vec3b pixel = image.at<Vec3b>(r, c);
-            printf("(%d %d %d) ", pixel[0], pixel[1], pixel[2]);
+            // Escala de grises
+            uchar gray_value = 0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0];
+
+
+            printf("(%d) ", gray_value);
         }
     }
 
